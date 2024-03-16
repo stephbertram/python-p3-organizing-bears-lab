8a317ac0e2b894fdc91336bc2f8b82b33cfed2cb
Stephanie Bertram <steph.a.bertram@gmail.com>
2024-03-16 08:29:36 -0700

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    ('Mr. Chocolate', 2, 'M', 'brown', 'grouchy', 1),
+    ('Rowdy', 2, 'M', 'brown', 'grouchy', 1),
+    ('Tabitha', 2, 'M', 'brown', 'grouchy', 1),
+    ('Sergeant Brown', 2, 'M', 'brown', 'grouchy', 1),
+    ('Melissa', 2, 'M', 'brown', 'grouchy', 1),
+    ('Grinch', 2, 'M', 'brown', 'grouchy', 1),
+    ('Wendy', 2, 'M', 'brown', 'grouchy', 1),
+    (NULL, 2, 'M', 'brown', 'grouchy', 1);
